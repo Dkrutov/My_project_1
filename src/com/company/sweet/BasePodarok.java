497d9a31caaa9d9487a632350462d8310b9f536c
Dmitrii Krutov <dm.krutov@mail.ru>
2023-05-10 23:35:42 +0300

    Задача Podarki v2

--- src/com/company/sweet/BasePodarok.java
+++ src/com/company/sweet/BasePodarok.java
@@ -1,4 +1,7 @@
 package com.company.sweet;
+
+import java.util.Scanner;
+
 /**
  * Created by Dmitriy on 21.02.23
  * Этот класс базовый для калькулятора
@@ -16,26 +19,94 @@ package com.company.sweet;
 
 public class BasePodarok {
     public static void main(String[] args) {
-        Candy candy1 = new Candy("Candy1", 1.0, 5.0, "011");
-        Chocolate chocolate1 = new Chocolate("Alpina", 2.0, 6.0, "021");
-        Chocolate chocolate2 = new Chocolate("Milka", 3.0, 7.0, "022");
-        Jellybean jellybean1 = new Jellybean("Vkus", 4.0, 8.0, "031");
-        Podarok[] box =  {candy1,chocolate1,chocolate2,jellybean1};
+        Scanner scanner = new Scanner(System.in);
+        Double sum = 0.00;
+        Double ves = 0.00;
+        System.out.println("Введите количество сладостей:");
+        int arrayCount = scanner.nextInt();
+        Podarok[] box = new Podarok[arrayCount];
+        int count=0;
+
+
+        System.out.println("Введите цифру, где: ");
+        System.out.println("'1' - Конфета 1");
+        System.out.println("'2' - Шоколад 2");
+        System.out.println("'3' - Мармелад 1");
+        System.out.println("'4' - Шоколад 1");
+
+
+        while ((arrayCount > 0)) {
+            int numSweet = scanner.nextInt();
+            System.out.println("Ожидание цифры");
+
+            switch (numSweet){
+                case 1:
+                    System.out.println("INFO: 1");
+                    Candy candy1 = new Candy("Candy1", 1.0, 5.0, "011");
+                    box[count]=candy1;
+                    System.out.println("INFO: count " + count);
+                    System.out.println("INFO: count " + box[count]);
+                    count=count + 1;
+                    arrayCount=arrayCount-1;
+                    break;
+                case 2:
+                    System.out.println("INFO: 2");
+                    Chocolate chocolate1 = new Chocolate("Milka", 5.0, 15.0, "022");
+                    box[count]=chocolate1;
+                    System.out.println("INFO: count " + count);
+                    System.out.println("INFO: count " + box[count]);
+                    count=count + 1;
+                    arrayCount=arrayCount-1;
+                    break;
+                case 3:
+                    System.out.println("INFO: 3");
+                    Jellybean jellybean1 = new Jellybean("Vkus", 4.0, 8.0, "031");
+                    box[count]=jellybean1;
+                    System.out.println("INFO: count " + count);
+                    System.out.println("INFO: count " + box[count]);
+                    count=count + 1;
+                    arrayCount=arrayCount-1;
+                    break;
+                case 4:
+                    System.out.println("INFO: 4");
+                    Chocolate chocolate2 = new Chocolate("Alpina", 2.0, 6.0, "021");
+                    box[count]=chocolate2;
+                    System.out.println("INFO: count " + count);
+                    System.out.println("INFO: count " + box[count]);
+                    count=count + 1;
+                    arrayCount=arrayCount-1;
+                    break;
+                default: System.out.println("Недобор сладостей! Выберите цифру 1-4");
+
+            }
+
+
+        }
+
+
+
+
+
+
+        int size = box.length;
+        System.out.println("INFO: size \n" + size);
+
         for (Podarok somePodarok : box) {
             System.out.println(somePodarok.toString());
         }
-        Double ves = 0.00;
-        int size = box.length;
+
+
         for (int i=0;i<size;i++) {
             ves = ves + box[i].weight;
         }
-        Double sum = 0.00;
+
 
         for (int i=0;i<size;i++) {
             sum = sum + box[i].price;
         }
-        System.out.println("Вес сладостей = " + ves);
-        System.out.println("Общая сумма = " + sum);
+
+        System.out.println("\nВес сладостей = " + ves);
+        System.out.println("\nОбщая сумма = " + sum);
 
 
     }
